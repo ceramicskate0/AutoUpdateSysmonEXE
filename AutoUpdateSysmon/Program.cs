533d3797508a1ee5106994bbea5a03bd8d52ba09
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2020-01-21 08:52:59 -0500

    Update Program.cs

--- AutoUpdateSysmon/Program.cs
+++ AutoUpdateSysmon/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -15,7 +15,47 @@ namespace AutoUpdateSysmon
 
         static void Main(string[] args)
         {
-            Wclient.DownloadFile(@"https://raw.githubusercontent.com/ceramicskate0/sysmon-config/master/sysmonconfig-export.xml", @"C:\Windows\sysmonconfig-export.xml");
+            try
+            {
+                if (args.Length <= 0)
+                {
+                    RunUpdate();
+                }
+                else
+                {
+                    if (args[0] == "-h" || args[0] == "h" || args[0] == "help" || args[0] == "-help" || args[0] == "/h" || args[0] == "/help")
+                    {
+                        Console.WriteLine(@"Args must be in this fomat:
+
+                                            AutoUpdateSysmon.exe 'URL of Sysmon Config' 'Host Computer File Path of Sysmon xml file'
+
+                                            Example: ./AutoUpdateSysmon.exe 'https://raw.githubusercontent.com/ceramicskate0/sysmon-config/master/sysmonconfig-export.xml' 'C:\Windows\sysmonconfig-export.xml'");
+                    }
+                    else
+                    {
+                        RunUpdate(args[0], args[1]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("App ERROR: Error == " + e.Message.ToString());
+                try
+                {
+                    RunUpdate();
+                }
+                catch
+                {
+                    Environment.Exit(1);
+                }
+            }
+
+
+
+        }
+        private static void RunUpdate(string URL= @"https://raw.githubusercontent.com/ceramicskate0/sysmon-config/master/sysmonconfig-export.xml", string HostLocation= @"C:\Windows\sysmonconfig-export.xml")
+        {
+            Wclient.DownloadFile(URL, HostLocation);
             ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Windows\Sysmon.exe", @"-c C:\Windows\sysmonconfig-export.xml");
             startInfo.WorkingDirectory = Path.GetDirectoryName(@"C:\Windows\");
             startInfo.RedirectStandardOutput = true;
